f48822b0b3ed506e9f501288672bb519a35b3789
unknown <hans.535220129@stu.untar.ac.id>
2023-11-15 15:08:50 +0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,332 @@
+import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Godrej',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Godrej'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color.fromARGB(255, 255, 0, 0),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          color: Colors.white,
+        ),
+        title: Text(
+          widget.title,
+          style: TextStyle(color: Colors.white),
+        ),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.search),
+            onPressed: () {
+              // Handle search icon press
+            },
+            color: Colors.white,
+          ),
+          IconButton(
+            icon: const Icon(Icons.shopping_bag),
+            onPressed: () {
+              // Handle shopping bag icon press
+            },
+            color: Colors.white,
+          ),
+        ],
+      ),
+      body: Stack(
+        children: [
+          // Subscribe section
+          Positioned(
+            top: 0,
+            left: 0,
+            right: 0,
+            child: ClipPath(
+              clipper: MyCustomClipper(),
+              child: Container(
+                height: 50,
+                color: Color.fromARGB(255, 255, 0, 0),
+              ),
+            ),
+          ),
+
+          // Image slider section
+          Positioned(
+            top: 200, // Adjust the position as needed
+            left: 0,
+            right: 0,
+            child: Container(
+              height: 150.0, // Set the desired height
+              child: CarouselSlider(
+                items: [
+                  // Add your local image widgets here
+                  // Example:
+                  Image.asset(
+                    'assets/image1.jpg',
+                    fit: BoxFit.cover, // Ensure the image is cropped to cover the container
+                  ),
+                  Image.asset(
+                    'assets/image2.jpg',
+                    fit: BoxFit.cover,
+                  ),
+                  Image.asset(
+                    'assets/image3.jpg',
+                    fit: BoxFit.cover,
+                  ),
+                ],
+                options: CarouselOptions(
+                  enlargeCenterPage: true,
+                  autoPlay: true,
+                  aspectRatio: 16 / 9, // Fixed aspect ratio
+                  autoPlayCurve: Curves.fastOutSlowIn,
+                  enableInfiniteScroll: true,
+                  autoPlayAnimationDuration: Duration(milliseconds: 800),
+                  viewportFraction: 0.9,
+                ),
+              ),
+            ),
+          ),
+
+          // Text below the carousel
+          Positioned(
+            top: 360, // Adjust the position as needed
+            left: 16,
+            child: Text(
+              'Belanja berdasarkan kategori',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 16,
+              ),
+            ),
+          ),
+
+          // Four squares
+          Positioned(
+            top: 400, // Adjust the position as needed
+            left: 16,
+            child: Row(
+              children: [
+                _buildSquare('assets/category_image1.jpg', 'Insecticides & Air Freshener'),
+                SizedBox(width: 8),
+                _buildSquare('assets/category_image2.jpg', 'Baby Wipes'),
+                SizedBox(width: 8),
+                _buildSquare('assets/category_image3.jpg', 'Hand Sanitizer & Antiseptic'),
+                SizedBox(width: 8),
+                _buildSquare('assets/category_image4.jpg', 'Household'),
+              ],
+            ),
+          ),
+
+          // Text below the squares
+          Positioned(
+            top: 510,
+            left: 16,
+            child: Text(
+              'Semua Produk',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 16,
+              ),
+            ),
+          ),
+
+          Column(
+            children: [
+              SizedBox(height: 30),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                child: SizedBox(
+                  height: 120,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.3),
+                          spreadRadius: 5,
+                          blurRadius: 7,
+                          offset: Offset(0, 3),
+                        ),
+                      ],
+                    ),
+                    child: Row(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: CircleAvatar(
+                            radius: 30,
+                            backgroundImage: NetworkImage('profilepicture.jpg'),
+                          ),
+                        ),
+                        SizedBox(width: 16),
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                'Godrej',
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              SizedBox(height: 8),
+                              Row(
+                                children: [
+                                  Icon(
+                                    Icons.person,
+                                    size: 16,
+                                    color: Colors.grey,
+                                  ),
+                                  SizedBox(width: 4),
+                                  Text(
+                                    '5,441 mengikuti',
+                                    style: TextStyle(
+                                      color: Colors.grey,
+                                      fontSize: 12,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              SizedBox(height: 8),
+                              Row(
+                                children: [
+                                  Expanded(
+                                    child: Padding(
+                                      padding: const EdgeInsets.only(right: 8.0),
+                                      child: OutlinedButton(
+                                        onPressed: () {
+                                          // Handle follow button press
+                                        },
+                                        style: OutlinedButton.styleFrom(
+                                          side: BorderSide(color: Colors.blue),
+                                          shape: RoundedRectangleBorder(
+                                            borderRadius: BorderRadius.only(
+                                              topLeft: Radius.circular(8.0),
+                                              bottomRight: Radius.circular(8.0),
+                                            ),
+                                          ),
+                                          minimumSize: Size(200, 36),
+                                        ),
+                                        child: Row(
+                                          mainAxisAlignment: MainAxisAlignment.center,
+                                          children: [
+                                            Text(
+                                              'Mengikuti',
+                                              style: TextStyle(
+                                                color: Colors.blue,
+                                                fontWeight: FontWeight.bold,
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                  Icon(
+                                    Icons.share,
+                                    color: Colors.blue,
+                                    size: 15,
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildSquare(String imagePath, String text) {
+    return Container(
+      width: 85,
+      height: 85,
+      decoration: BoxDecoration(
+        color: const Color.fromARGB(255, 224, 224, 224),
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(15.0),
+          bottomRight: Radius.circular(15.0),
+        ),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset(
+            imagePath,
+            width: 40,
+            height: 40,
+            fit: BoxFit.cover,
+          ),
+          SizedBox(height: 8),
+          Text(
+            text,
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.grey,
+              fontSize: 12,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class MyCustomClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    Path path = Path();
+    path.lineTo(0, size.height);
+    path.lineTo(size.width - 20, size.height);
+    path.quadraticBezierTo(size.width, size.height, size.width, size.height - 20);
+    path.lineTo(size.width, 0);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(covariant CustomClipper<Path> oldClipper) {
+    return false;
+  }
+}
